6590b64645c31cab46b5decbee04bca9e567b9fc
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2025-10-27 00:59:31 -0700

    Update game.gd

--- open-the-portal/game.gd
+++ open-the-portal/game.gd
@@ -8,9 +8,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#print(Engine.get_frames_per_second())
 	pass
 
 
 func _on_character_body_2d_2_enemy_on_screen(enemy_position: Vector2) -> void:
 	print(enemy_position)
+	# We need to update this position when the enemy moves 
 	$CharacterBody2D.enemy_pos = enemy_position
