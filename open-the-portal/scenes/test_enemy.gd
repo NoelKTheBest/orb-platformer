f99d080ceeb08bb6fc9ec73586adf0c3ae6ff7e7
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2025-10-27 00:58:05 -0700

    Add Player Detection for Enemy

--- open-the-portal/scenes/test_enemy.gd
+++ open-the-portal/scenes/test_enemy.gd
@@ -2,9 +2,14 @@ extends CharacterBody2D
 
 signal enemy_on_screen(enemy_position : Vector2)
 
+var player_position
+## Distance from the player where enemy starts attacking
+@export var attack_distance : int = 30
+@export var speed = 2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player_position = position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,6 +18,11 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
+	var target_position = (player_position - position)
+	var distance_to = position.distance_squared_to(player_position)
+	
+	velocity.x = target_position.x * speed
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
@@ -23,8 +33,35 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	#print("an orb is close")
 	$AnimationPlayer.stop()
 	$AnimationPlayer.play("block")
-	
+
+
+func get_player_position(player_pos : Vector2):
+	player_position = player_pos
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	enemy_on_screen.emit(position)
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Orbs"):
+		die()
+
+
+func die():
+	queue_free()
+
+
+func _on_player_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_position = body.position
+
+
+func _on_player_detection_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_position = position
+
+
+func _on_player_attack_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		$AnimationPlayer.play("block")
