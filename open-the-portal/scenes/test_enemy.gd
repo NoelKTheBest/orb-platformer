57ff91ab1c3df80de112a307202c796b11eb78b5
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2025-10-28 16:06:37 -0700

    Update test_enemy.gd

--- open-the-portal/scenes/test_enemy.gd
+++ open-the-portal/scenes/test_enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 signal enemy_on_screen()
 
+var monitor_player_position
 var player_position
 ## Distance from the player where enemy starts attacking
 @export var attack_distance : int = 30
@@ -15,38 +16,37 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if !$AnimationPlayer.is_playing(): $AnimationPlayer.play("idle")
+	if velocity.x < 0: $Sprite2D.flip_h = true
+	elif velocity.x > 0: $Sprite2D.flip_h = false
 
 
 func _physics_process(delta: float) -> void:
-	var target_position = (player_position - position)
-	var distance_to = position.distance_squared_to(player_position)
-	
-	velocity.x = target_position.normalized().x * speed
-	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	if monitor_player_position:
+		var direction
+		var target_position = (player_position - position).normalized()
+		var distance_to = position.distance_squared_to(player_position)
+		
+		velocity.x = target_position.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
 	# We need a better way to detect the player's direction relative to the enemy
 	# Copy code from warrior script in wombo combo
-	if velocity.x > 0:
+	if velocity.x != 0:
 		$AnimationPlayer.play("run")
 	elif velocity.x == 0:
 		$AnimationPlayer.play("idle")
 	
 	move_and_slide()
-	
-	if target_position.x < 0: $Sprite2D.flip_h = true
-	elif target_position.x > 0: $Sprite2D.flip_h = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	#print("an orb is close")
 	$AnimationPlayer.stop()
 	$AnimationPlayer.play("block")
-
-
-func get_player_position(player_pos : Vector2):
-	player_position = player_pos
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
@@ -64,12 +64,12 @@ func die():
 
 func _on_player_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		player_position = body.position
+		monitor_player_position = true
 
 
 func _on_player_detection_area_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		player_position = position
+		monitor_player_position = false
 
 
 func _on_player_attack_area_body_entered(body: Node2D) -> void:
