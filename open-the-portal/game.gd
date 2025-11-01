c3b94fffe92cbb24866e54895aa80dd6bfa5dca2
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2025-10-31 20:17:04 -0700

    ADD second enemy type

--- open-the-portal/game.gd
+++ open-the-portal/game.gd
@@ -3,16 +3,18 @@ extends Node2D
 @onready var player: CharacterBody2D = $Player
 
 var monitor_enemies : bool
+var enemies_in_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print(get_tree().get_nodes_in_group("Enemy"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if monitor_enemies: player.enemy_pos = $Enemy.position
-	$Enemy.player_position = player.position
+	if monitor_enemies: player.enemy_pos = $Jumping_Enemy.position
+	if $Jumping_Enemy: $Jumping_Enemy.player_position = player.position
+	#print(get_tree().get_nodes_in_group("Enemy"))
 
 
 func _on_enemy_enemy_on_screen() -> void:
@@ -25,3 +27,16 @@ func position_debug():
 	var c = str($Enemy.player_position)
 	var formatted_string: String = a + 'n/' + b + 'n/' + c + 'n/'
 	return formatted_string
+
+
+func _on_jumping_enemy_enemy_on_screen() -> void:
+	pass # Replace with function body.
+
+
+func enemy_is_visible():
+	# 
+	pass
+
+
+func enemy_is_dead():
+	pass
---
+++ open-the-portal/scenes/test_enemy_2.gd
@@ -0,0 +1,109 @@
+extends CharacterBody2D
+
+signal enemy_on_screen()
+signal enemy_died()
+
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+
+var monitor_player_position
+var player_position
+var attacking :  bool
+var on_cooldown : bool
+## Distance from the player where enemy starts attacking
+@export var attack_distance : int = 30
+@export var speed = 2
+@export var jump_velocity = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player_position = position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !anim_player.is_playing(): anim_player.play("idle")
+	if velocity.x < 0: sprite.flip_h = true
+	elif velocity.x > 0: sprite.flip_h = false
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# When the player enters the first bubble, enemy movement is triggered
+	#	The enemy will always move when the player is inside the first bubble
+	if monitor_player_position:
+		var direction
+		var target_position = (player_position - position).normalized()
+		var distance_to = position.distance_squared_to(player_position)
+		
+		velocity.x = target_position.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	# When the player enters the second bubble, the enemy begins it's attack cycle
+	#	The enemy will continuously attack the player with a breif cooldown in between 
+	#	when the player is inside the second bubble.
+	if !attacking:
+		if velocity.x != 0:
+			anim_player.play("run")
+		elif velocity.x == 0:
+			anim_player.play("idle")
+	else:
+		if !on_cooldown:
+			anim_player.play('block')
+	
+	if velocity.y > 0: anim_player.play("fall")
+	
+	move_and_slide()
+
+
+func die():
+	enemy_died.emit()
+	queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	enemy_on_screen.emit()
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Orbs"):
+		die()
+
+
+func _on_player_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		monitor_player_position = true
+
+
+func _on_player_detection_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		monitor_player_position = false
+
+
+func _on_player_attack_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		attacking = true
+
+
+func _on_player_attack_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		attacking = false
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "block":
+		$Timer.start()
+		on_cooldown = true
+
+
+func _on_timer_timeout() -> void:
+	on_cooldown = false
+
+
+func _on_attack_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Orbs") and is_on_floor():
+		velocity.y = jump_velocity
+		anim_player.play("jump")
