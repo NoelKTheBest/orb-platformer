87bc9e15bb64923c6eb45af7f0350bd494eb0b2a
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2025-11-13 11:25:59 -0800

    ADD forcefield

---
+++ open-the-portal/prototype_2/prototype_2_player.gd
@@ -0,0 +1,97 @@
+extends CharacterBody2D
+
+@onready var orb_spawn_position: Node2D = $OrbSpawnPosition
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var camera_follow: Node2D = $CameraFollow
+@onready var camera_2d: Camera2D = $CameraFollow/Camera2D
+
+## The distance from the enemy at which the camera will focus on the player
+@export var player_camera_focus_range : float = 132551.375
+
+signal orb_was_fired
+signal player_died
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const ORB_VELOCITY = 400
+
+var orb = preload("res://scenes/orb.tscn")
+var enemy_pos : Vector2
+var on_cooldown = false
+
+
+func _process(delta: float) -> void:
+	if enemy_pos: 
+		move_camera(delta)
+	
+	if Input.is_action_just_pressed("fire") and !on_cooldown:
+		var new_orb = orb.instantiate()
+		
+		# Set properties before node is ready to have access to them
+		new_orb.position = orb_spawn_position.position
+		var aim_dir = Vector2(1, 0) if sprite_2d.flip_h == false else Vector2(-1, 0)
+		new_orb.linear_v = aim_dir.normalized() * ORB_VELOCITY
+		add_child(new_orb)
+		$SpawnTimer.start()
+		on_cooldown = true
+		orb_was_fired.emit()
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func move_camera(_delta : float):
+	if enemy_pos and enemy_pos != position:
+		#t += delta * 0.4
+		var _line_to_enemy : Vector2 = enemy_pos - position
+		var m = (enemy_pos.y - position.y)/(enemy_pos.x - position.x)
+		var x_mid = (enemy_pos.x - position.x)/2
+		var y = x_mid * m
+		camera_follow.position = Vector2(x_mid, y)
+		#print(position.distance_squared_to(enemy_pos))
+		#if position.distance_squared_to(enemy_pos) < close_zoom_range:
+			#if !is_zoomed_close: $CameraFollow/AnimationPlayer.play("close_zoom")
+			#is_zoomed_close = true
+		#else:
+			#if is_zoomed_close: $CameraFollow/AnimationPlayer.play("normal_zoom")
+			#is_zoomed_close = false
+		
+		# if there is only one enemy in the scene
+		#	focus on enemy if they are close enough
+		if position.distance_squared_to(enemy_pos) > player_camera_focus_range:
+			camera_follow.position = Vector2.ZERO
+	else:
+		camera_follow.position = Vector2.ZERO
+
+
+func reset_camera_follow():
+	enemy_pos = position
+
+
+func _on_hurtbox_player_was_hit() -> void:
+	die()
+
+
+func die():
+	player_died.emit()
+
+
+func _on_spawn_timer_timeout() -> void:
+	on_cooldown = false
