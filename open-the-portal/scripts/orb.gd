4f95975f22512daa05b41833e6e48b5e7a93032d
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2025-11-19 12:14:40 -0800

    Update Collision Mask on Orb

--- open-the-portal/scripts/orb.gd
+++ open-the-portal/scripts/orb.gd
@@ -4,21 +4,8 @@ extends RigidBody2D
 @onready var linear_v
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
-	#apply_force(launch_vector)
-	#apply_central_force()
-	print(linear_velocity)
-
-
-func _on_body_shape_entered(_body_rid: RID, _body: Node, _body_shape_index: int, _local_shape_index: int) -> void:
-	print("shape collision")
-	#launch_vector.x *= -1
-
-
 func _on_body_entered(_body: Node) -> void:
-	print("collision")
-	#launch_vector.x * -1
+	queue_free()
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
@@ -27,7 +14,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_tree_entered() -> void:
 	linear_velocity = linear_v
-	#print(linear_v)
 
 
 func _on_timer_timeout() -> void:
