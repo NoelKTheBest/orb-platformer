615ccd7d6da0527e371b2625eecf965925544d05
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2025-12-01 23:17:33 -0800

    Update orb.gd

--- open-the-portal/scripts/orb.gd
+++ open-the-portal/scripts/orb.gd
@@ -4,6 +4,14 @@ extends RigidBody2D
 @onready var linear_v
 
 
+func _process(_delta: float) -> void:
+	if get_colliding_bodies().size() > 0:
+		queue_free()
+	
+	if get_contact_count() > 0:
+		queue_free()
+
+
 func _on_body_entered(_body: Node) -> void:
 	queue_free()
 
@@ -17,4 +25,12 @@ func _on_tree_entered() -> void:
 
 
 func _on_timer_timeout() -> void:
+	queue_free()
+
+
+func _on_body_shape_entered(_body_rid: RID, _body: Node, _body_shape_index: int, _local_shape_index: int) -> void:
+	queue_free()
+
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	queue_free()
