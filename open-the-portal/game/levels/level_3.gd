52e567e4cceecc62deee6cc400a16c96113caa14
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2026-04-09 18:51:22 -0700

    Add Camera to Level 3

---
+++ open-the-portal/game/levels/level_3.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$LevelCamera.make_current()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func pause_camera() -> void:
+	$LevelAnimations.pause()
+	await get_tree().create_timer(1.0).timeout
+	$LevelAnimations.play()
