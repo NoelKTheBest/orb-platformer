a38e6a60bf26bc78c8aaae35ae8a13070cd70552
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2026-06-04 18:27:45 -0700

    Update navigatable_entity.gd

--- open-the-portal/characters/navigatable_entity.gd
+++ open-the-portal/characters/navigatable_entity.gd
@@ -1,6 +1,16 @@
 @abstract extends ControlledEntity
 
-@export var enemy_count = 5
+var target_position: Vector2
+var monitor_player_position = false
+var player_position = Vector2.ZERO
+var patrol_area = true
+var camera_position := Vector2.ZERO
+var sound_source_position := Vector2.ZERO
+var heat_sensor_position := Vector2.ZERO
+var nearest_door_position := Vector2.ZERO
+var current_floor : int
+var using_door: bool = false
+var destination_floor: int
+var listen_for_player_coords = false
 
-func a():
-	pass
+func normalize_target_position(): return target_position.normalized()
