98e40e0cbbfa5bb7ded01536a16fd2700244d276
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2026-06-06 20:14:01 -0700

    Update basic_entity.gd

--- open-the-portal/characters/basic_entity.gd
+++ open-the-portal/characters/basic_entity.gd
@@ -1,1 +1,29 @@
 @abstract extends NavigatableEntity
+
+var is_sprite_flipped := false
+var hitbox_init_pos : Vector2
+var flip_scale = -1
+## Determines whether a player is nearby. Must be used with AnimationTree StateMachine Node
+var player_nearby := false
+
+
+func _process(_delta: float) -> void:
+	if velocity.x < 0: 
+		is_sprite_flipped = true
+		#$Hitbox.position = Vector2(-18, 3)
+		#if $VisibilityArea: $VisibilityArea.scale.x = -1
+	elif velocity.x > 0: 
+		is_sprite_flipped = false
+		#$Hitbox.position = Vector2(18, 3)
+		#if $VisibilityArea: $VisibilityArea.scale.x = 1
+
+
+func _physics_process(delta: float) -> void:
+	super(delta)
+	
+	# Call function to update player_nearby and other states
+	update_state()
+
+
+## Function to override when changing any state variables for the entity
+@abstract func update_state()
