1ae80a747ce2661fbbad13afe1979b86d1e21993
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2026-06-08 02:43:25 -0700

    Add patrol area functionality and return to squad position function for NavigatableEntity

--- open-the-portal/characters/navigatable_entity.gd
+++ open-the-portal/characters/navigatable_entity.gd
@@ -2,8 +2,16 @@
 ## This class is meant to provide functions and some basic implementation to 
 ## allow the entity to choose a position to go towards
 
-## Check this when you want the enemy to simply patrol an area. Overrides [b]monitor_player_position[/b]
+## Determines if entity should walk from position to position in an area and
+## waiting to see if the player enters their FOV. Overrides [b]monitor_player_position[/b]
 @export var patrol_area = true
+## Determines if entity should stay in position as part of a squadron. Overrides [b]patrol_area[/b]
+@export var on_gaurd := true
+#@export var patrol_rect: Rect2
+## Start position for patrol area
+@export var patrol_start_x: float = 0.0
+## End position for patrol area
+@export var patrol_end_x: float = 1.0
 
 #var target_position: Vector2
 var monitor_player_position = false
@@ -17,8 +25,19 @@ var using_door: bool = false
 var destination_floor: int
 var listen_for_player_coords = false
 
+var squad_position
+var patrol_target_position_x
+
+var player_out_of_range: bool = true
 
 #func normalize_target_position(): return target_position.normalized()
+
+
+## Sets [b]squad_position[/b] to current position if [b]on_gaurd[/b] is set to true
+func _ready() -> void:
+	super()
+	
+	if on_gaurd: squad_position = position
 
 
 ## Function to implement to set the target position of the entity based on position relative to player
@@ -34,3 +53,19 @@ func get_target_position() -> Vector2:
 			target_position = (player_position - position).normalized()
 	
 	return target_position
+  
+
+## If [b]patrol_area[/b] is set to true, checks if the entity has reached the end of the patrolling area
+func check_for_end_of_area():
+	# If position surpasses the bounds of the area, 
+	# 	set target position to opposite side of area
+	if position.x > patrol_end_x:
+		patrol_target_position_x = patrol_start_x
+	elif position.x < patrol_start_x:
+		patrol_target_position_x = patrol_end_x
+
+
+## Send the position the enemy should move to if the player is out of reach of the entity
+func return_to_squad_position() -> Vector2:
+	if player_out_of_range: return squad_position
+	else: return Vector2.ZERO # Will be used to check if the entity should continue pursuing the player
